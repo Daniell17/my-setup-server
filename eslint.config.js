fc0d88b0d32583d91854416cca23c41ac066a1e8
daniellgjoka77 <93450828+Daniell17@users.noreply.github.com>
2025-12-15 21:49:40 +0100

    build fix

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require('@eslint/js')
+const tsParser = require('@typescript-eslint/parser')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+
+module.exports = [
+  { ignores: ['dist', 'node_modules'] },
+  {
+    files: ['src/**/*.{ts,js}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
